d1ef46f6803648d0748ed64e394042fa8de291b6
KARTIKKUMAR SANGADA <kartiksangada2004@gmail.com>
2025-11-11 20:54:09 +0530

    Add files via upload

---
+++ second_largest.py
@@ -0,0 +1,17 @@
+numbers = []
+
+n = input("How many numbers do you want to enter: ")
+
+for i in range (n):
+    num = int(input(f"Enter number{i+1}: "))
+    numbers.append(n)
+
+if len(numbers) < 2:
+    print("Not enough numbers to determine the second largest.")
+else:
+    unique_numbers = list(set(numbers))
+    unique_numbers.sort(reverse=True)
+    
+    second_largest = unique_numbers[1]
+    print("The second largest number is:", second_largest)
+    
