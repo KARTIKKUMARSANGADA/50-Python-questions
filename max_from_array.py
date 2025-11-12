c9faf32a67cb0100787dbf40a19e2b569aa56aeb
KARTIKKUMAR SANGADA <kartiksangada2004@gmail.com>
2025-11-12 22:14:27 +0530

    Python Program to Find Largest Element in an Array

---
+++ max_from_array.py
@@ -0,0 +1,8 @@
+arr = []
+n = int(input("Enter the number of elements in the array: "))
+for i in range(0, n):
+    ele = int(input(f"Enter element:{i+1}: "))
+    arr.append(ele) 
+    
+ans = max(arr)
+print('maximum from array:', ans)
